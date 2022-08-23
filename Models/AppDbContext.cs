215f7ab863b83504d024fa9981d733d72b748129
Kiss Bence <kissbence19@gmail.com>
2022-08-23 19:34:52 +0200

    Modifying AppDbContext

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -10,21 +10,26 @@ namespace DadsInventory.Models
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Item> Items { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
 
-            modelBuilder.Entity<Category>().HasData(new Category {CategoryId = 4, CategoryName = "Large"});
-            modelBuilder.Entity<Category>().HasData(new Category {CategoryId = 5, CategoryName = "Medium"});
-            modelBuilder.Entity<Category>().HasData(new Category {CategoryId = 6, CategoryName = "Small"});
+            modelBuilder.Entity<Category>().HasData(new Category { CategoryId = 4, CategoryName = "Large" });
+            modelBuilder.Entity<Category>().HasData(new Category { CategoryId = 5, CategoryName = "Medium" });
+            modelBuilder.Entity<Category>().HasData(new Category { CategoryId = 6, CategoryName = "Small" });
 
             modelBuilder.Entity<Item>().HasData(
-                new Item {Id = 1, Name = "Ship from fb", InStock = true, CategoryId = 4, Price = 199.95m},
-                new Item {Id = 2, Name = "Hammer from db", InStock = false, CategoryId = 5, Price = 7.95m},
-                new Item {Id = 3, Name = "Pin from db", InStock = true, CategoryId = 6, Price = 1.95m}
+                new Item { Id = 1, Name = "Ship from fb", InStock = true, CategoryId = 4, Price = 199.95m },
+                new Item { Id = 2, Name = "Hammer from db", InStock = false, CategoryId = 5, Price = 7.95m },
+                new Item { Id = 3, Name = "Pin from db", InStock = true, CategoryId = 6, Price = 1.95m }
             );
+
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Email = "dad@dad.com", Password = "dad", Role = "Dad", Username = "Dad" },
+                new User { Id = 2, Email = "mom@mom.com", Password = "mom", Role = "Mom", Username = "Mom" });
         }
     }
 }

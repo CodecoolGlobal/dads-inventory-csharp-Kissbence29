3b216b722d77745719d91fc9fb254e7ae39b9536
Kiss Bence <kissbence19@gmail.com>
2022-08-23 19:40:54 +0200

    Creating user repository

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using DadsInventory.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DadsInventory.Repositories
+{
+    public class UserRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public UserRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public Task<User> AuthenticateAsync(string userName, string password) => Task.FromResult(_appDbContext.Users.First(user => user.Username == userName && user.Password == password));
+    }
+}
